224f40e1471d29593bb1ef8ae1c563919b1434e2
jehaddayman <jehaddayman@gmail.com>
2025-07-27 18:46:56 +0300

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,13 +1,11 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-const Header = () => {
+export default function Header() {
   return (
-    <header className="bg-blue-600 text-white p-4 flex gap-4">
-      <Link href="/home" className="hover:underline">Home</Link>
-      <Link href="/about" className="hover:underline">About</Link>
-      <Link href="/posts" className="hover:underline">Posts</Link>
+    <header className="bg-gray-800 text-white p-4 flex space-x-4">
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/posts">Posts</Link>
     </header>
   );
-};
-
-export default Header;
+}
