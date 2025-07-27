3137d2b5f22740ed00a772b33bc3842947540e2b
jehaddayman <jehaddayman@gmail.com>
2025-07-27 18:15:27 +0300

    Create Header.tsx

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-500 text-white p-4">
+      <h2 className="text-xl">ALX Header</h2>
+    </header>
+  );
+};
+
+export default Header;
