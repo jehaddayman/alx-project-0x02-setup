8ef82e7bdd49fefb64b6367d4cbbdf981089b469
jehaddayman <jehaddayman@gmail.com>
2025-07-27 18:14:53 +0300

    Create index.tsx

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/layout/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className="flex min-h-screen items-center justify-center">
+        <h1 className="text-3xl font-bold text-center">Welcome to ALX Project 2 🚀</h1>
+      </main>
+    </>
+  );
+}
