6e081a035cd67d486dca3defacc112cfbfefc335
jehaddayman <jehaddayman@gmail.com>
2025-07-27 18:34:46 +0300

    Update about.tsx

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,7 +1,14 @@
+import Button from "@/components/common/Button";
+
 const AboutPage = () => {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold">About Us</h1>
+      <h1 className="text-2xl font-bold mb-4">About Us</h1>
+      <div className="flex gap-4">
+        <Button size="small" shape="rounded-sm" label="Small" />
+        <Button size="medium" shape="rounded-md" label="Medium" />
+        <Button size="large" shape="rounded-full" label="Large" />
+      </div>
     </div>
   );
 };
