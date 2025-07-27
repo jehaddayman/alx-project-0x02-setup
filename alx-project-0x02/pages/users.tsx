c55230622611c2ffc15b42dded2a9d6068354db7
jehaddayman <jehaddayman@gmail.com>
2025-07-27 19:15:12 +0300

    Update users.tsx

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,23 +1,13 @@
 // pages/users.tsx
-import React from 'react';
+import { GetStaticProps } from 'next';
 import { UserProps } from '../interfaces';
 import UserCard from '../components/common/UserCard';
 
-interface UsersPageProps {
+type Props = {
   users: UserProps[];
-}
-
-const UsersPage = ({ users }: UsersPageProps) => {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
-      {users.map((user) => (
-        <UserCard key={user.id} user={user} />
-      ))}
-    </div>
-  );
 };
 
-export const getStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const users: UserProps[] = await res.json();
 
@@ -26,6 +16,16 @@ export const getStaticProps = async () => {
       users,
     },
   };
+};
+
+const UsersPage = ({ users }: Props) => {
+  return (
+    <div className="grid gap-4 p-4 md:grid-cols-2 lg:grid-cols-3">
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+  );
 };
 
 export default UsersPage;
