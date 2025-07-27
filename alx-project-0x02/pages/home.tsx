2a02a8eece9f0ac3e3ffe5b8ef9865ab6d0ad90c
jehaddayman <jehaddayman@gmail.com>
2025-07-27 18:20:30 +0300

    Create home.tsx

---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
